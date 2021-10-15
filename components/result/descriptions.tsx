3365312ca58470751a8d745421082cf42a52bcd7
01Joseph-Hwang10 <joseph95501@gmail.com>
2021-10-15 21:49:23 +0900

    change texts

--- components/result/descriptions.tsx
+++ components/result/descriptions.tsx
@@ -1,6 +1,19 @@
-import { gmmCopyrights, kMeansCopyrights } from "@src/data/cluster-copyrights";
+import {
+  gmmCopywritings,
+  kMeansCopywritings,
+} from "@src/data/cluster-copywritings";
 import { ClusterType } from "@src/data/cluster.types";
 import React from "react";
+import MarkdownText from "./descriptions/markdown-text";
+
+const renderDescription = (
+  description: string,
+  index: number
+): React.ReactNode => {
+  return (
+    <MarkdownText key={`description-${index.toString()}`} text={description} />
+  );
+};
 
 interface DescriptionsProps {
   kMeansResult: string;
@@ -15,32 +28,23 @@ const Descriptions: React.FC<DescriptionsProps> = ({
 }) => {
   const resultDetail =
     mode === "k-means"
-      ? kMeansCopyrights.filter(
+      ? kMeansCopywritings.filter(
         (copyright) => copyright.clusterLabel === kMeansResult // eslint-disable-line
       )[0] // eslint-disable-line
-      : gmmCopyrights.filter(
+      : gmmCopywritings.filter(
         (copyright) => copyright.clusterLabel === gmmResult // eslint-disable-line
       )[0]; // eslint-disable-line
-  const regex = /(".*")/.exec(resultDetail.title);
-  const title = regex[1];
-  const subtitle = resultDetail.title.replace(title, "");
   return (
     <div className="flex flex-col justify-start items-center">
       <div className="my-5 w-full justify-center items-center flex">
-        <span style={titleStyle}>{title}</span>
+        <span style={titleStyle}>{resultDetail.title}</span>
       </div>
       <div className="mb-10 w-full justify-center items-center flex">
-        <span style={subTitleStyle}>{subtitle}</span>
+        <span style={subTitleStyle}>{resultDetail.subtitle}</span>
       </div>
       <div className="flex justify-center items-center w-full">
         <ul className="w-11/12 flex flex-col justify-start items-center">
-          {resultDetail.descriptions.map((description, index) => {
-            return (
-              <li key={index} className="list-disc w-9/12">
-                <span style={listTitleStyle}>{description}</span>
-              </li>
-            );
-          })}
+          {resultDetail.descriptions.map(renderDescription)}
         </ul>
       </div>
     </div>
@@ -70,17 +74,5 @@ const subTitleStyle: React.CSSProperties = {
   lineHeight: 1.11,
   letterSpacing: "normal",
   textAlign: "center",
-  color: "#191919",
-};
-
-const listTitleStyle: React.CSSProperties = {
-  fontFamily: "NanumSquareR",
-  fontSize: "16px",
-  fontWeight: "normal",
-  fontStretch: "normal",
-  fontStyle: "normal",
-  lineHeight: 1.5,
-  letterSpacing: "normal",
-  textAlign: "left",
   color: "#191919",
 };
---
+++ components/result/descriptions/markdown-text.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+interface MarkdownTextProps {
+  text: string;
+}
+
+type Token = "-" | "#";
+
+const tokens: Token[] = ["-", "#"];
+
+const MarkdownText: React.FC<MarkdownTextProps> = ({ text }) => {
+  const token = text.substring(0, 1);
+  let textToRender: string;
+  if (tokens.includes(token as Token)) {
+    textToRender = text.substring(1);
+  } else {
+    textToRender = text;
+  }
+  switch (token) {
+    case "-": // eslint-disable-line
+      return <ListTitle text={textToRender} />; // eslint-disable-line
+    case "#": // eslint-disable-line
+      return <BoldTitle text={textToRender} />; // eslint-disable-line
+    default: // eslint-disable-line
+      return <DefaultText text={textToRender} />; // eslint-disable-line
+  }
+};
+
+const BoldTitle: React.FC<MarkdownTextProps> = ({ text }) => {
+  return (
+    <li className="list-none w-9/12">
+      <span>&quot; {text} &quot;</span>
+    </li>
+  );
+};
+
+const ListTitle: React.FC<MarkdownTextProps> = ({ text }) => {
+  return (
+    <li className="list-none w-9/12">
+      <span style={listTitleStyle}>{text}</span>
+    </li>
+  );
+};
+
+const DefaultText: React.FC<MarkdownTextProps> = ({ text }) => {
+  return <span style={listTitleStyle}>{text}</span>;
+};
+
+export default MarkdownText;
+
+const listTitleStyle: React.CSSProperties = {
+  fontFamily: "NanumSquareR",
+  fontSize: "16px",
+  fontWeight: "normal",
+  fontStretch: "normal",
+  fontStyle: "normal",
+  lineHeight: 1.5,
+  letterSpacing: "normal",
+  textAlign: "left",
+  color: "#191919",
+};
